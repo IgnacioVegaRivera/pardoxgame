7016faae31d82af7fe4ad90a7d64fbc20a31dec9
IgnacioVegaRivera <144045715+IgnacioVegaRivera@users.noreply.github.com>
2026-01-08 22:22:42 -0600

    Added Maze Scene
    - Added in base of the maze in the demo scene
    - Preparing for advancing the teleportation movement method

--- vr_game_personal/Scripts/finalDestination/arrowMove.gd
+++ vr_game_personal/Scripts/finalDestination/arrowMove.gd
@@ -1,8 +1,11 @@
 extends XROrigin3D
 
-@onready var arrow : CSGCylinder3D = get_parent().get_node("ArrowPad")
+@onready var arrow : CSGCylinder3D = get_parent().get_node("Depth Assests").get_node("ArrowPad")
 @onready var pointer : RayCast3D = get_node("Right/RayCast3D")
-@onready var groundArea : Area3D = get_parent().get_node("Floor/FloorArea")
+@onready var groundArea : Area3D = get_parent().get_node("Depth Assests").get_node("Floor").get_node("FloorArea")
+#@onready var arrow : CSGCylinder3D = get_parent().get_node("ArrowPad")
+#@onready var pointer : RayCast3D = get_node("Right/RayCast3D")
+#@onready var groundArea : Area3D = get_parent().get_node("Floor/FloorArea")
 var cursor_location = Vector3.ZERO
 var right_joystick = Vector2.ZERO
 
